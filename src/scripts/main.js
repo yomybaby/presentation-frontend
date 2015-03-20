8bd4d44b01be99b835a094abaddcfc496bc6a39f
Jong Eun Lee <yomybaby@yomybaby.com>
2015-03-20 19:33:35 +0900

    front기술에 대한 공유 자료 v1.0 (first commit)

---
+++ src/scripts/main.js
@@ -0,0 +1,52 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  cube = require('bespoke-theme-cube'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress'),
+  state = require('bespoke-state'),
+  forms = require('bespoke-forms'),
+  run = require('bespoke-run');
+
+// Bespoke.js
+bespoke.from('article', [
+  cube(),
+  keys(),
+  touch(),
+  bullets('.bullet'),
+  scale(),
+  hash(),
+  progress(),
+  state(),
+  forms(),
+  run()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
+(function preloadBackgroundImages() {
+
+  var matches, image,
+    forEach = function(arrayLike, fn) {
+      [].slice.call(arrayLike, 0).forEach(fn);
+    };
+
+  forEach(document.styleSheets, function(sheet) {
+    forEach(sheet.rules, function(rule) {
+      if (rule.style && rule.style.backgroundImage) {
+        matches = rule.style.backgroundImage.match(/url\((.*)\)/);
+        if (matches) {
+          image = new Image();
+          image.src = matches[1];
+        }
+      }
+    });
+  });
+
+}());
